ba9bac18f839bceed65af1e3acdc2acd70624cc9
karolpasierb <lefanu@vivaldi.net>
2018-10-28 11:38:53 +0000

    WIP Added Test GIntegrate

---
+++ Testing_GIntegrate/TestGIntegrate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CSPlang;
+using PlugAndPlay;
+
+namespace Testing_GIntegrate
+{
+    class TestGIntegrate
+    {
+        static void Main(string[] args)
+        {
+            One2OneChannel N2I = Channel.one2one();
+            One2OneChannel I2P = Channel.one2one();
+
+            IamCSProcess[] testList = {new Numbers(outChannel: N2I.Out ()),
+            new Integrate(inChannel: N2I.In (),
+            outChannel: I2P.Out ()),
+            new GPrint(inChannel: I2P.In (),
+            heading: "Integrate")
+                };
+
+            new CSPParallel(testList).run();
+        }
+    }
+}
