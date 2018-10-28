4b2c005ea09436c35e00f1489c39ea9d0c05bc81
karolpasierb <lefanu@vivaldi.net>
2018-10-28 11:06:03 +0000

    Initial commit

---
+++ CSPsharp-UCaPE_Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSPsharp_UCaPE_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
