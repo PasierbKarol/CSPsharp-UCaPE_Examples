6c6ef65cf4ec18e6f30a7aa154e34fa69608ff2b
karolpasierb <lefanu@vivaldi.net>
2018-11-12 10:34:35 +0000

    Updated Testing Gintegrate. Works as expected

--- Testing_GIntegrate/TestGIntegrate.cs
+++ Testing_GIntegrate/TestGIntegrate.cs
@@ -11,12 +11,12 @@ namespace Testing_GIntegrate
             One2OneChannel N2I = Channel.one2one();
             One2OneChannel I2P = Channel.one2one();
 
-            IamCSProcess[] testList = {new Numbers(outChannel: N2I.Out ()),
-            new Integrate(inChannel: N2I.In (),
-            outChannel: I2P.Out ()),
-            new GPrint(inChannel: I2P.In (),
-            heading: "Integrate")
-                };
+            IamCSProcess[] testList = 
+            {
+                new Numbers(outChannel: N2I.Out ()),
+                new Integrate(In: N2I.In (),Out: I2P.Out ()),
+                new GPrint(inChannel: I2P.In (), heading: "Integrate")
+            };
 
             new CSPParallel(testList).run();
         }
