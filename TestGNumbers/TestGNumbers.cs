775abe2f73f516aff2886e45b71488157184eebe
karolpasierb <lefanu@vivaldi.net>
2018-11-15 14:35:12 +0000

    Added project TestGNumber and it's fully functional

---
+++ TestGNumbers/TestGNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CSPlang;
+using PlugAndPlay;
+
+namespace TestGNumbers
+{
+    class TestGNumbers
+    {
+        static void Main(string[] args)
+        {
+            One2OneChannel N2P = Channel.one2one();
+
+            IamCSProcess[] testList =
+            {
+                new Numbers(outChannel: N2P.Out ()),
+                new GPrint(inChannel: N2P.In (), heading: "Numbers", delay: 1000)
+            };
+
+            new CSPParallel(testList).run();
+        }
+    }
+}
